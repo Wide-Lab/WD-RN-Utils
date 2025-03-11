63e7ea5feaa2ba93e3f4dc68870bffb5617635ff
Widelab - Leonardo <leonardo.noro@widelab.com.br>
2025-03-11 14:07:50 -0300

    Update dependencies and enhance shadowGenerator function with WDNumbers interpolation

--- src/Styles.ts
+++ src/Styles.ts
@@ -1,5 +1,5 @@
-import { ViewStyle } from "react-native"
-import { Numbers } from "widelab-utils"
+import { ViewStyle } from 'react-native';
+import { Numbers, WDNumbers } from 'widelab-utils';
 
 /**
  * Generates a shadow style object for a React Native component based on the specified depth.
@@ -7,36 +7,35 @@ import { Numbers } from "widelab-utils"
  * such as color, offset, opacity, and radius, which are calculated using interpolation based on the depth.
  *
  * @param depth - The desired depth level for the component's shadow.
- * @returns {ViewStyle} An object containing the shadow styles for the component.
+ * @returns An object containing the shadow styles for the component.
  */
 export const shadowGenerator = (depth: number): ViewStyle => {
-  const elevation = Numbers.numberClamp(depth, 1, 24)
+  const inputRange: [number, number] = [1, 24];
+  const elevation = Numbers.numberClamp(depth, ...inputRange);
 
   const blurList = [
     1, 2, 4, 5, 8, 10, 10, 10, 12, 14, 15, 17, 19, 21, 22, 24, 26, 28, 29, 31,
     33, 35, 36, 38,
-  ]
+  ];
 
-  function interpolate(
-    i: number,
-    a: number,
-    b: number,
-    a2: number,
-    b2: number
-  ) {
-    return ((i - a) * (b2 - a2)) / (b - a) + a2
-  }
+  const floorHeight = Math.floor(elevation * 0.5);
+  const height = Math.max(1, floorHeight);
+  const base = elevation - 1;
+  const blur = blurList[base];
 
-  const floorHeight = Math.floor(elevation * 0.5)
-  const height = Math.max(1, floorHeight)
-  const base = elevation - 1
-  const blur = blurList[base]
+  const shadowOpacityBase = WDNumbers.interpolate(
+    base,
+    ...inputRange,
+    0.2,
+    0.6
+  );
+  const shadowOpacity = Number(shadowOpacityBase.toFixed(2));
 
-  const shadowOpacity = Number(interpolate(base, 1, 24, 0.2, 0.6).toFixed(2))
-  const shadowRadius = Number(interpolate(blur, 1, 38, 1, 16).toFixed(2))
+  const shadowRadiusBase = WDNumbers.interpolate(blur, 1, 38, 1, 16);
+  const shadowRadius = Number(shadowRadiusBase.toFixed(2));
 
   return {
-    shadowColor: "#000000",
+    shadowColor: '#000000',
     shadowOffset: {
       width: 0,
       height,
@@ -45,5 +44,5 @@ export const shadowGenerator = (depth: number): ViewStyle => {
     shadowRadius,
 
     elevation,
-  }
-}
+  };
+};
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import * as WDStyles from './Styles';
+
+export { WDStyles };
